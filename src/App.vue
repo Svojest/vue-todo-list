53938d235605debd4ef81ed569c4a8d8f39b6e61
Svojest <svojest@yandex.ru>
2022-09-17 15:09:05 +0700

    feat: v1 BREAKING CHANGE: added adding tasks and deleting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todo-container">
+    <h1 class="todo-header">Твой TO DO</h1>
+
+    <div class="add">
+      <input
+        type="text"
+        placeholder="Добавить новую задачу"
+        class="add__input"
+        maxlength="35"
+        v-model="tasks.name"
+        @keypress.enter="addTask"
+      >
+      <button
+        class="add-button"
+        @click="addTask"
+      >+</button>
+    </div>
+
+    <ul class="task-list">
+      <li
+        class="task-list__item"
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <label class="task-list__item-label">
+          <input type="checkbox">
+          <span>{{task.name}}</span>
+        </label>
+        <button
+          class="delete-button"
+          @click="removeTask(task)"
+        ></button>
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      tasks: [
+        { name: 'Первая задча', id: 1 },
+        { name: 'Velit aliquip nostrud est consequat proident dolor Lorem ea ex consectetur dolore consectetur do ut.', id: 2 },
+        { name: 'Третья задча', id: 3 },
+        { name: 'Четвертая задча', id: 4 },
+        { name: 'Пятая задча', id: 5 },
+        { name: 'Шестая задча', id: 6 },
+      ]
+
+
+    }
+  },
+  methods: {
+    addTask() {
+      if (!this.tasks.name) {
+        return
+      }
+      this.tasks.push({
+        name: this.tasks.name,
+      })
+      this.tasks.name = ''
+    },
+    removeTask(task) {
+      this.tasks.splice(this.tasks.indexOf(task), 1)
+    }
+  },
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
